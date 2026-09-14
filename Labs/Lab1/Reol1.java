a0b4978bad4a925d3e83d1167cfca01c424ce1e3
limin195 <mingkei.li@gmail.com>
2026-09-13 22:17:58 -0400

    changes

---
+++ Labs/Lab1/Reol1.java
@@ -0,0 +1,6 @@
+public class Reol1{
+    public static void main(String[] args) {
+        String [] name = {"John", "Mary", "Peter", "Sally"};
+        System.out.println(name[0].charAt(0));
+    }
+}
